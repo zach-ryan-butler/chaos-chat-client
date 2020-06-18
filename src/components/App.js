27705debb4078417c8068889b89272798ad7ac7c
Zach Butler <zach.standley93@gmail.com>
2020-06-18 14:46:10 -0700

    created a button that will toggle the display on the client socket component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-const ENDPOINT = 'http://127.0.0.1:4001';
+import React, { useState } from 'react';
+import ClientSocket from './ClientSocket';
 
 export default function App() {
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    console.log(socket);
-    socket.on('FromAPI', data => {
-      setResponse(data);
-    });
-  }, []);
+  const [loadClient, setLoadClient] = useState(true);
 
   return (
-    <p>
-      It&apos;s <time dateTime={response}>{response}</time>
-    </p>
+    <>
+      <button onClick={() => setLoadClient(prevState => !prevState)}>
+        Click Me
+      </button>
+      {loadClient ? <ClientSocket /> : null}
+    </>
   );
 }
