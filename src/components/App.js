260acba47c68483bdffe3d636093ebb5af9ece8e
Zach Butler <zach.standley93@gmail.com>
2020-06-18 12:29:56 -0700

    built out time display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://127.0.0.1:4001';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    console.log(socket);
+    socket.on('FromAPI', data => {
+      setResponse(data);
+    });
+  }, []);
+
+  return (
+    <p>
+      It&apos;s <time dateTime={response}>{response}</time>
+    </p>
+  );
 }
-  
