bda44272a384259692929eb6099569717bb5ba3a
Zach Butler <zach.standley93@gmail.com>
2020-06-18 14:45:46 -0700

    moved the clients socket connection logic to its own component

---
+++ src/components/ClientSocket.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://127.0.0.1:4001';
+
+export default function ClientSocket() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('FromAPI', data => {
+      setResponse(data);
+    });
+
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <p>
+      It&apos;s <time dateTime={response}>{response}</time>
+    </p>
+  );
+}
